f7845d5e6216df2d4366950a8a299315f50172fa
Vee <narayanan.venkiteswaran@appdirect.com>
2014-04-09 17:51:58 -0700

    documentation

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 #First time
-1. Get the list of files boostrap csvs - smb://batcave/engineering/hackathon-Q2-2014/bugseer/bootstrap_csvs/bugseer-csv.tar.gz and put in the ${project.home}
+1. Get the list of files boostrap csvs and extract it to the ${project.home} - smb://batcave/engineering/hackathon-Q2-2014/bugseer/bootstrap_csvs/bugseer-csv.tar.gz
 2. run scripts/create_db.sql
-   mysql -h HOST -u USER -pPASSWORD < scripts/create_db.sql
+   mysql --local-infile -h HOST -u USER -pPASSWORD < scripts/create_db.sql
 3. mvn install:install-file -Dfile=lib/org.moxieapps.gwt.highcharts-1.5.0.jar -DgroupId=org.moxieapps.gwt -DartifactId=highcharts -Dversion=1.5.0 -Dpackaging=jar
 
 #Package and deploy
 1. mvn clean -U gwt:clean compile gwt:resources gwt:compile package -DskipTests=true -Dgwt.module=com.bugseer.BugSeer
-2. Copy the directory target/bugseer into your tomcat webapps directory, restart tomcat
+2. Copy the directory target/bugseer into your tomcat webapps directory
+3. Change the app.properties in $TOMCAT_HOME/webapps/bugseer/WEB-INF/classes/app.properties and add your git credentials under git.username and git.password
+4. restart tomcat
--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -1,13 +1,13 @@
--- Dumping database structure for bug_predictor
-CREATE DATABASE IF NOT EXISTS `bug_predictor` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `bug_predictor`;
+-- Dumping database structure for bugseer
+CREATE DATABASE IF NOT EXISTS `bugseer` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `bugseer`;
  
  
 CREATE TABLE IF NOT EXISTS `commit_filenames` (
   `sha_ts` int NOT NULL,
   `sha` varchar(40) NOT NULL,
   `filename` varchar(512) NOT NULL,
-  `created_ts` int NOT NULL
+  `created_ts` int NOT NULL,
   KEY `filename_sha_ix` (`sha`),
   KEY `filename_sha_ts_ix` (`sha_ts`),
   KEY `filename_filename_ix` (`filename`)
@@ -27,26 +27,31 @@ CREATE TABLE IF NOT EXISTS `jira` (
   KEY `jira_jira_ix` (`jira`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `weights` (
+CREATE TABLE IF NOT EXISTS `weights` (
 	`a` INT(10) NOT NULL,
 	`b` INT(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+TRUNCATE TABLE commit_filenames;
+TRUNCATE TABLE commit_jira;
+TRUNCATE TABLE jira;
+TRUNCATE TABLE weights;
+
 LOAD DATA LOCAL INFILE 'files.csv' INTO TABLE commit_filenames FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' (sha_ts, sha, filename);
 LOAD DATA LOCAL INFILE 'commits.csv' INTO TABLE commit_jira FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' (sha, jira);
-LOAD DATA LOCAL INFILE 'jira.csv' INTO TABLE jira FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (jira, priority, created, updated);
+LOAD DATA LOCAL INFILE 'jira.csv' INTO TABLE jira FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (jira, priority, updated);
 
 update commit_filenames cf inner join (select filename, min(sha_ts) ts from commit_filenames group by filename) cf1 on cf1.filename = cf.filename set cf.created_ts = cf1.ts;
 insert into weights values( 12,12 );
 
-SET group_concat_max_len = 12000;
+SET GLOBAL group_concat_max_len = 12000;
 CREATE OR REPLACE VIEW vw_score_normalized_time AS
 SELECT 
  SUBSTRING_INDEX(cf.filename, '/', -1) "filename",
  SUM(1/(1+ EXP((select a from weights)-(select b from weights)*(cf.sha_ts - cf.created_ts)/(unix_timestamp(NOW()) -  cf.created_ts)))) "score",
  group_concat((cf.sha_ts - cf.created_ts)/(unix_timestamp(NOW()) -  cf.created_ts) ORDER BY cf.sha_ts SEPARATOR ':') "x",
  group_concat(round(1/(1+ EXP((select a from weights)-(select b from weights)*((cf.sha_ts - cf.created_ts)/(unix_timestamp(NOW()) -  cf.created_ts)))),4) ORDER BY cf.sha_ts SEPARATOR ':') "y",
- group_concat(cj.jira ORDER BY ORDER BY cf.sha_ts SEPARATOR ':' "jira",
+ group_concat(cj.jira ORDER BY cf.sha_ts SEPARATOR ':') "jira",
  count(j.jira) "num_bugs"
 FROM commit_filenames cf, commit_jira cj, jira j
 WHERE j.jira = cj.jira AND cj.sha = cf.sha
@@ -59,7 +64,7 @@ SELECT
  SUM(1/(1+ EXP((select a from weights)-(select b from weights)*(cf.sha_ts - cf.created_ts)/(unix_timestamp(NOW()) -  cf.created_ts)))) "score",
  group_concat(cf.sha_ts ORDER BY cf.sha_ts SEPARATOR ':') "x",
  group_concat(round(1/(1+ EXP((select a from weights)-(select b from weights)*((cf.sha_ts - cf.created_ts)/(unix_timestamp(NOW()) -  cf.created_ts)))),4) ORDER BY cf.sha_ts SEPARATOR ':') "y",
- group_concat(cj.jira ORDER BY ORDER BY cf.sha_ts SEPARATOR ':' "jira",
+ group_concat(cj.jira ORDER BY cf.sha_ts SEPARATOR ':') "jira",
  count(j.jira) "num_bugs"
 FROM commit_filenames cf, commit_jira cj, jira j
 WHERE j.jira = cj.jira AND cj.sha = cf.sha
